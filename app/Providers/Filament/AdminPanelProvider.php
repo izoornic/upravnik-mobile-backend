d8262cf857ab467e85f4620b9f44c7ebb57ec02a
izoornic <izornic@gmail.com>
2026-01-21 20:29:47 +0100

    v 1.0.5

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
-                AdminMiddleware::class,
+                //AdminMiddleware::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
