21c01092aa796453d10c77f137f9cebaea921e00
izoornic <izornic@gmail.com>
2026-04-03 11:24:45 +0200

    V.1.0.8

--- app/Http/Resources/V1/StanAnalitikaResource.php
+++ app/Http/Resources/V1/StanAnalitikaResource.php
@@ -9,17 +9,12 @@ class StanAnalitikaResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        $stariDug = $this->stari_dug;
-        $meseci   = $this->meseci ?? [];
-
-        $mesecSum = fn (string $field) => collect($meseci)->sum(fn (array $m) => (float) $m[$field]);
-
         return [
             'stan_id' => $this->sid,
             'ukupno'  => [
-                'zaduzeno'  => $mesecSum('zaduzeno')  + ($stariDug ? (float) $stariDug['zaduzeno']  : 0),
-                'razduzeno' => $mesecSum('razduzeno') + ($stariDug ? (float) $stariDug['razduzeno'] : 0),
-                'saldo'     => $mesecSum('saldo')     + ($stariDug ? (float) $stariDug['saldo']     : 0),
+                'zaduzeno'  => (float) $this->zaduzeno,
+                'razduzeno' => (float) $this->razduzeno,
+                'saldo'     => (float) $this->saldo,
             ],
         ];
     }
