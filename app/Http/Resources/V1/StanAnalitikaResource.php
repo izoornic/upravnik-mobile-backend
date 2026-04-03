6db9e6ad39ab236a5d16c4b1b5dc8d55c69f76e0
izoornic <izornic@gmail.com>
2026-04-03 11:02:05 +0200

    V.0.1.7

--- app/Http/Resources/V1/StanAnalitikaResource.php
+++ app/Http/Resources/V1/StanAnalitikaResource.php
@@ -14,38 +14,13 @@ class StanAnalitikaResource extends JsonResource
 
         $mesecSum = fn (string $field) => collect($meseci)->sum(fn (array $m) => (float) $m[$field]);
 
-        $ukupnoZaduzeno  = $mesecSum('zaduzeno')  + ($stariDug ? (float) $stariDug['zaduzeno']  : 0);
-        $ukupnoRazduzeno = $mesecSum('razduzeno') + ($stariDug ? (float) $stariDug['razduzeno'] : 0);
-        $ukupnoSaldo     = $mesecSum('saldo')     + ($stariDug ? (float) $stariDug['saldo']     : 0);
-
         return [
             'stan_id' => $this->sid,
-            'ukupno' => [
-                'zaduzeno'  => $ukupnoZaduzeno,
-                'razduzeno' => $ukupnoRazduzeno,
-                'saldo'     => $ukupnoSaldo,
+            'ukupno'  => [
+                'zaduzeno'  => $mesecSum('zaduzeno')  + ($stariDug ? (float) $stariDug['zaduzeno']  : 0),
+                'razduzeno' => $mesecSum('razduzeno') + ($stariDug ? (float) $stariDug['razduzeno'] : 0),
+                'saldo'     => $mesecSum('saldo')     + ($stariDug ? (float) $stariDug['saldo']     : 0),
             ],
-            'stari_dug' => $stariDug ? [
-                'zaduzeno'  => $stariDug['zaduzeno'],
-                'razduzeno' => $stariDug['razduzeno'],
-                'saldo'     => $stariDug['saldo'],
-            ] : null,
-            'meseci' => collect($meseci)->map(fn (array $m) => [
-                'mid'          => $m['mid'],
-                'datum'        => $m['datum'],
-                'year'         => $m['y_no'],
-                'mesec_naziv'  => $m['m_naziv'],
-                'zaduzeno'     => $m['zaduzeno'],
-                'razduzeno'    => $m['razduzeno'],
-                'saldo'        => $m['saldo'],
-                'payment_date' => $m['r_date'],
-                'stari_dug'    => (bool) $m['stari_dug'],
-                'preknjizen'   => (bool) $m['preknjizen'],
-                'preknjizeni'  => collect($m['preknjizeni'] ?? [])->map(fn (array $p) => [
-                    'date'   => $p['r_date'],
-                    'iznos'  => $p['r_iznos'],
-                ])->values()->all(),
-            ])->values()->all(),
         ];
     }
 }
